cf209bcbf0a6e12f35f97cec78363c9c27512d03
streude <scott@treude.net>
2025-09-16 14:59:43 -0400

    added copying of segmentation files

--- generators/ift.py
+++ generators/ift.py
@@ -1,15 +1,26 @@
 import os
+import glob
+import shutil
 from fontTools.ttLib import TTFont
-from testCaseGeneratorLib.paths import IFTSourcePath, IFTTestDirectory
+from testCaseGeneratorLib.paths import IFTSourcePath, IFTTestDirectory, resourcesDirectory
 
 
 # TODO:  Need to create sub-directory for each IFT because they are multiple files
 if not os.path.exists(IFTTestDirectory):
     os.makedirs(IFTTestDirectory)
 
-testDirectory = os.path.join(IFTTestDirectory, "test_name_goes_here");
+
+testDirectory = os.path.join(IFTTestDirectory, "test_name_goes_here")
 if not os.path.exists(testDirectory):
     os.makedirs(testDirectory)
+
+# Copy _gk and _tk files from resources/IFT/ to testDirectory
+source_dir = os.path.join(resourcesDirectory, "IFT")
+for pattern in ("*_gk", "*_tk"):
+    for file_path in glob.glob(os.path.join(source_dir, pattern)):
+        shutil.copy(file_path, testDirectory)
+        print(f"Copied {file_path} to {testDirectory}")
+
 outPath = os.path.join(testDirectory, "myfont-mod.ift.otf");
 font = TTFont(IFTSourcePath)
 

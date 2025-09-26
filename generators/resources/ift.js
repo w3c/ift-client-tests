bd6c076002d013731201e475ef2dbe2a4bc54a86
streude <scott@treude.net>
2025-09-26 15:42:25 -0400

    added cache-buster for IFT file

--- generators/resources/ift.js
+++ generators/resources/ift.js
@@ -6,7 +6,8 @@ async function update_all_fonts() {
   const resultElements = document.getElementsByClassName('result');
   for (let el of resultElements) {
     let test_name = el.id;
-    let title_font = `${test_name}/myfont-mod.ift.otf`;
+    let rndNum = Math.floor(Math.random() * 100000); // to break caching
+    let title_font = `${test_name}/myfont-mod.ift.otf?v=${rndNum}`;
     let title_text = document.getElementById(test_name).innerText;
     let font_name = test_name + " IFT Font";
     // check to see if element contains pass or fail class

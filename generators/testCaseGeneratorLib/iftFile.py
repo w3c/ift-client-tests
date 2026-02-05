32bc8a434c3c0e3aab312e2db2c04d0a710f485c
streude <scott@treude.net>
2026-02-05 09:31:01 -0500

    moved IFT file class to library

---
+++ generators/testCaseGeneratorLib/iftFile.py
@@ -0,0 +1,47 @@
+import os
+import glob
+import shutil
+from fontTools.ttLib import TTFont
+from testCaseGeneratorLib.paths import resourcesDirectory,  clientTestDirectory
+
+class IFTFile:
+    def __init__(self, testName,format,fontFileName):
+        self.testName = testName 
+        self.format = format
+        self.fontFileName = fontFileName
+        self.testDirectory = os.path.join(clientTestDirectory, testName)
+        self.sourceFontPath = os.path.join(resourcesDirectory, "IFT", format, "font.ift.woff2")
+        self.font = TTFont(self.sourceFontPath)
+        self.createTestDirectory()
+        self.copyIFTSourceFiles()
+    def createTestDirectory(self):
+        if not os.path.exists(self.testDirectory):
+            os.makedirs(self.testDirectory)
+    def copyIFTSourceFiles(self):
+        # Copy _gk and _tk files from resources/IFT/ to testDirectory
+        sourceDir = os.path.join(resourcesDirectory, "IFT",self.format)
+        destDir = os.path.join(self.testDirectory,self.format)
+        if not os.path.exists(destDir):
+            os.makedirs(destDir)
+        for pattern in ("*_gk", "*_tk"):
+            for filePath in glob.glob(os.path.join(sourceDir, pattern)):
+                shutil.copy(filePath, destDir)
+                print(f"Copied {filePath} to {destDir}")
+    def getIFTTableData(self):
+        if "IFT " not in self.font:
+            raise ValueError("IFT table not found in font.")
+        # Unknown/custom tables are stored as raw bytes on .data
+        self.tbl = self.font["IFT "]
+        self.raw = bytearray(self.tbl.data)
+        return self.raw
+    def setIFTTableData(self, data):
+        self.raw = bytearray(data)
+    def removeTable(self,tableTag):
+        del self.font[tableTag]
+    def writeTestIFTFile(self):
+        if self.tbl and self.raw:
+            self.tbl.data = bytes(self.raw)
+        outPath = os.path.join(self.testDirectory,self.format,  self.fontFileName)
+        if not os.path.exists(os.path.join(self.testDirectory,self.format)):
+            os.makedirs(os.path.join(self.testDirectory,self.format))
+        self.font.save(outPath)

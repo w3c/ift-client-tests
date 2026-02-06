917be56271c8180a4ce405dea8994e0441028e32
streude <scott@treude.net>
2026-02-06 12:03:57 -0500

    latest hope

--- generators/utilities/readMappingEntries.py
+++ generators/utilities/readMappingEntries.py
@@ -1,4 +1,6 @@
 import sys
+import struct
+import pprint
 from pathlib import Path
 from typing import List, Dict, Union
 
@@ -40,77 +42,87 @@ def encoder_filename(entry_index: int) -> str:
     # fallback: sequential hex if out of mapping
     return f"{entry_index:02X}.ift_tk"
 
-# -----------------------
-# Parse Format 2 IFT table
-# -----------------------
-def parse_format2_ift_table(ift_data: bytes) -> List[Dict[str, Union[str,int,List[int]]]]:
+def read_uint24(data, offset):
+    return int.from_bytes(data[offset:offset+3], "big"), offset + 3
+
+def parse_format2_table(data: bytes):
     offset = 0
-    data_len = len(ift_data)
-    entries: List[Dict[str, Union[str,int,List[int]]]] = []
+    table_start = 0  # offsets are relative to start of this table
 
-    # --- Header ---
-    format_byte = ift_data[offset]
-    offset += 1
-    if format_byte != 2:
-        raise ValueError("Only Format 2 supported")
-    offset += 3  # reserved
-    flags = ift_data[offset]
+    # uint8 format
+    format_ = data[offset]
     offset += 1
 
-    offset += 16  # skip compatibility ID
-    default_patch_format = ift_data[offset]
+    # uint24 reserved
+    reserved, offset = read_uint24(data, offset)
+
+    # uint8 flags
+    flags = data[offset]
     offset += 1
-    entry_count_bytes = ift_data[offset:offset+3]
-    entryCount = int.from_bytes(entry_count_bytes, byteorder='big')
-    offset += 3
 
-    # --- Parse entries ---
-    for entry_index in range(entryCount):   
-        print(entry_index);
-#         entry_format = ift_data[offset]
-#         offset += 1
-# 
-#         url_len = ift_data[offset]
-#         offset += 1
-#         offset += url_len  # skip per-entry URL bytes
-# 
-#         # patch data: until next 0x00 or 0x01 byte (simple heuristic)
-#         next_entry_offset = offset
-#         while next_entry_offset < data_len and ift_data[next_entry_offset] not in (0x00,0x01):
-#             next_entry_offset += 1
-#         patch_data = ift_data[offset:next_entry_offset]
-#         offset = next_entry_offset
+    has_cff = bool(flags & 0x01)
+    has_cff2 = bool(flags & 0x02)
 
-        # decode glyphs
-#         glyphs = decode_sparse_bitset(patch_data) if entry_format == 1 else list(range(len(patch_data)))
+    # uint32[4] compatibilityId
+    compatibility_ids = struct.unpack_from(">4I", data, offset)
+    offset += 16
 
-#         url = encoder_filename(entry_index)
+    # uint8 defaultPatchFormat
+    default_patch_format = data[offset]
+    offset += 1
 
-#         entries.append({
-#             "entryFormat": entry_format,
-#             "url": url,
-#             "patchFormat": default_patch_format,
-#             "glyphs": glyphs,
-#             "patchBytes": patch_data.hex()
-#         })
+    # uint24 entryCount
+    entry_count, offset = read_uint24(data, offset)
 
+    # Offset32 entries
+    entries_offset = struct.unpack_from(">I", data, offset)[0]
+    offset += 4
 
-#    return entries
+    # Offset32 entryIdStringData
+    entry_id_string_data_offset = struct.unpack_from(">I", data, offset)[0]
+    offset += 4
 
+    # uint16 urlTemplateLength
+    url_template_length = struct.unpack_from(">H", data, offset)[0]
+    offset += 2
+
+    # uint8[urlTemplateLength] urlTemplate
+    url_template_bytes = data[offset:offset + url_template_length]
+    offset += url_template_length
+
+    # Optional uint32 cffCharStringsOffset
+    cff_charstrings_offset = None
+    if has_cff:
+        cff_charstrings_offset = struct.unpack_from(">I", data, offset)[0]
+        offset += 4
+
+    # Optional uint32 cff2CharStringsOffset
+    cff2_charstrings_offset = None
+    if has_cff2:
+        cff2_charstrings_offset = struct.unpack_from(">I", data, offset)[0]
+        offset += 4
+
+    return {
+        "format": format_,
+        "reserved": reserved,
+        "flags": flags,
+        "hasCFF": has_cff,
+        "hasCFF2": has_cff2,
+        "compatibilityId": compatibility_ids,
+        "defaultPatchFormat": default_patch_format,
+        "entryCount": entry_count,
+        "entriesOffset": entries_offset,
+        "entryIdStringDataOffset": entry_id_string_data_offset,
+        "urlTemplateLength": url_template_length,
+        "urlTemplate": url_template_bytes,
+        "cffCharStringsOffset": cff_charstrings_offset,
+        "cff2CharStringsOffset": cff2_charstrings_offset,
+        "nextOffset": offset,  # where Mapping Entries table begins if sequential
+    }
 # -----------------------
 # Load IFT table
 # -----------------------
 iftFile = IFTFile("exampleTestFile", "GLYF", "myfont-mod.ift.woff2")
 ift_data = iftFile.getIFTTableData()
 
-patch_entries = parse_format2_ift_table(ift_data)
-
-# -----------------------
-# Print entries
-# -----------------------
-for idx, e in enumerate(patch_entries):
-    print(f"Entry {idx}:")
-    print("  url:", e["url"])
-    print("  patchFormat:", e["patchFormat"])
-    print("  glyphs:", e["glyphs"])
-    print()
+pprint.pprint(parse_format2_table(ift_data))
